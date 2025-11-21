340f20c1369b9fa138a1e8b13202586c93fb17cd
Shameel333 <110900866+Shameel333@users.noreply.github.com>
2025-11-22 00:05:12 +0530

    Refactor service worker event listeners
    Refactored service worker to improve readability and maintainability.

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("daymate-cache").then(cache => {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./style.css",
-                "./script.js",
-                "./manifest.json",
-                "./icons/icon-192.png",
-                "./icons/icon-512.png"
-            ]);
-        })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("daymate-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png",
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
 });
+
+
